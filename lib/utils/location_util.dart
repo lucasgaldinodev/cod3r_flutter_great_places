133f3dc8ef46855191032a97ff84d9e17e1a7bf9
Lucas Galdino <lucasgaldino7@gmail.com>
2022-07-08 10:00:14 -0300

    correção api key

--- lib/utils/location_util.dart
+++ lib/utils/location_util.dart
@@ -2,8 +2,9 @@ import 'dart:convert';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
+import 'package:great_places/.env' as env;
 
-const googleApiKey = 'AIzaSyA28g8n4ukONZq6MtsMZnc9siDICASvD_0';
+final googleApiKey = env.googleApiKey;
 
 class LocationUtil {
   static String generateLocationPreviewImage({
